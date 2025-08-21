492c87fd8bdf045d75324e1a2efa613e02564502
Jacob Keil <57192057+JacobKeil@users.noreply.github.com>
2025-08-21 12:38:17 -0400

    restructure

---
+++ .svelte-kit/types/src/routes/events/[id]/proxy+page.server.ts
@@ -0,0 +1,34 @@
+// @ts-nocheck
+import { db } from '$lib/server/database'
+import type { PageServerLoad } from './$types'
+
+export const load = async ({ locals: { session }, cookies, params }: Parameters<PageServerLoad>[0]) => {
+  const event = await db.event.findFirst({
+    where: {
+      id: params.id
+    },
+    include: {
+      main_broadcast: true,
+      watch_parties: true,
+      teams: {
+        orderBy: {
+          name: 'asc'
+        },
+        include: {
+          created_by: true,
+          channels: true,
+          region: true
+        }
+      },
+      created_by: true,
+      links: true,
+      status: true
+    }
+  })
+
+  return {
+    session,
+    cookies: cookies.getAll(),
+    event
+  }
+}
