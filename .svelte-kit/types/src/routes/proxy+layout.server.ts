58c4d819a5791ed5f2a78c910bb063adb2c1eb18
Jacob Keil <57192057+JacobKeil@users.noreply.github.com>
2025-09-18 16:49:56 -0400

    add new global supabase file

--- .svelte-kit/types/src/routes/proxy+layout.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { LayoutServerLoad } from './$types'
-
-export const load = async ({ locals: { session }, cookies }: Parameters<LayoutServerLoad>[0]) => {  
-  return {
-    session,
-    cookies: cookies.getAll()
-  }
-}
